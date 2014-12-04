b4dc9f24d2468332295c12a1f9435a53ed60aa35
Harsh <shahmharsh@gmail.com>
2014-12-04 11:44:37 -0800

    Added dropbox wrapper from dropbox API

--- SharePic/HRDropbox.h
+++ SharePic/HRDropbox.h
@@ -7,7 +7,16 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import <DropboxSDK/DropboxSDK.h>
+#import "HRConstants.h"
 
 @interface HRDropbox : NSObject
+
++ (id)sharedDropbox;
+- (BOOL)isLoggedIn;
+- (BOOL)handleOpenURL:(NSURL *)url;
+- (void)loginWithController:(UIViewController *)viewController;
+- (void)logout;
 
 @end
--- SharePic/HRDropbox.m
+++ SharePic/HRDropbox.m
@@ -10,4 +10,41 @@
 
 @implementation HRDropbox
 
++ (id)sharedDropbox {
+    
+    static HRDropbox *sharedDropbox = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sharedDropbox = [[self alloc] init];
+    });
+    return sharedDropbox;
+}
+
+-(id) init {
+    if (self = [super init]) {
+        DBSession *dbSession = [[DBSession alloc]
+                                initWithAppKey:HRDropBoxAppKey
+                                appSecret:HRDropBoxAppSecret
+                                root:kDBRootAppFolder];
+        [DBSession setSharedSession:dbSession];
+    }
+    return self;
+}
+
+- (BOOL)handleOpenURL:(NSURL *)url {
+    return [[DBSession sharedSession] handleOpenURL:url];
+}
+
+- (BOOL)isLoggedIn {
+    return [[DBSession sharedSession] isLinked];
+}
+
+- (void)loginWithController:(UIViewController *)viewController {
+    [[DBSession sharedSession] linkFromController:viewController];
+}
+
+- (void)logout {
+    [[DBSession sharedSession] unlinkAll];
+}
+
 @end
